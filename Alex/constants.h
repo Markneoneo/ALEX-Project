614b1fef65db1fd90f11d8e65e5c5d8b92df7f83
Markneoneo <e1155612@u.nus.edu>
2024-04-09 12:05:24 +0800

    Update constants.h
    Updated dir

--- Alex/constants.h
+++ Alex/constants.h
@@ -45,4 +45,21 @@ typedef enum
   COMMAND_HUMP = 9
 } TCommandType;
 
+typedef enum 
+{ 
+FORWARD=1, 
+BACKWARD=2, 
+LEFT=3, 
+RIGHT=4 
+} TDirection; 
+
+typedef enum Tdir
+{
+  STOP,
+  GO,
+  BACK,
+  CCW,
+  CW
+} Tdir;
+
 #endif
