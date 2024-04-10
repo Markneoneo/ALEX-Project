9cc86c4fcc51fe22f41057833696daf7c5231453
Betahaxer <108223321+Betahaxer@users.noreply.github.com>
2024-04-10 14:34:20 +0800

    Create alex-pi.cpp

---
+++ TCP/Server/alex-pi.cpp
@@ -0,0 +1,728 @@
+#include <stdio.h>
+
+#include <pthread.h>
+
+#include <semaphore.h>
+
+#include <unistd.h>
+
+#include <stdint.h>
+
+#include "packet.h"
+
+#include "serial.h"
+
+#include "serialize.h"
+
+#include "constants.h"
+
+
+
+#include <iostream>
+
+#include <cstring>
+
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+#include <arpa/inet.h> // For inet_addr
+
+#include <csignal>     // For signal handling
+
+
+
+#define PORT_NAME			"/dev/ttyACM0"
+
+#define BAUD_RATE			B9600
+
+#define PI_IP "192.168.88.162"
+
+
+
+int exitFlag=0;
+
+sem_t _xmitSema;
+
+
+
+void handleError(TResult error)
+
+{
+
+	switch(error)
+
+	{
+
+		case PACKET_BAD:
+
+			printf("ERROR: Bad Magic Number from ARDUINO\n");
+
+			break;
+
+
+
+		case PACKET_CHECKSUM_BAD:
+
+			printf("ERROR: Bad checksum\n");
+
+			break;
+
+
+
+		default:
+
+			printf("ERROR: UNKNOWN ERROR\n");
+
+	}
+
+}
+
+
+
+void handleStatus(TPacket *packet)
+
+{
+
+	printf("\n ------- ALEX STATUS REPORT ------- \n\n");
+
+	printf("Left Forward Ticks:\t\t%d\n", packet->params[0]);
+
+	printf("Right Forward Ticks:\t\t%d\n", packet->params[1]);
+
+	printf("Left Reverse Ticks:\t\t%d\n", packet->params[2]);
+
+	printf("Right Reverse Ticks:\t\t%d\n", packet->params[3]);
+
+	printf("Left Forward Ticks Turns:\t%d\n", packet->params[4]);
+
+	printf("Right Forward Ticks Turns:\t%d\n", packet->params[5]);
+
+	printf("Left Reverse Ticks Turns:\t%d\n", packet->params[6]);
+
+	printf("Right Reverse Ticks Turns:\t%d\n", packet->params[7]);
+
+	printf("Forward Distance:\t\t%d\n", packet->params[8]);
+
+	printf("Reverse Distance:\t\t%d\n", packet->params[9]);
+
+	printf("\n---------------------------------------\n\n");
+
+}
+
+
+
+void handleResponse(TPacket *packet)
+
+{
+
+	// The response code is stored in command
+
+	switch(packet->command)
+
+	{
+
+		case RESP_OK:
+
+			printf("Command OK\n");
+
+		break;
+
+
+
+		case RESP_STATUS:
+
+			handleStatus(packet);
+
+		break;
+
+
+
+		default:
+
+			printf("Arduino is confused\n");
+
+	}
+
+}
+
+
+
+void handleErrorResponse(TPacket *packet)
+
+{
+
+	// The error code is returned in command
+
+	switch(packet->command)
+
+	{
+
+		case RESP_BAD_PACKET:
+
+			printf("Arduino received bad magic number\n");
+
+		break;
+
+
+
+		case RESP_BAD_CHECKSUM:
+
+			printf("Arduino received bad checksum\n");
+
+		break;
+
+
+
+		case RESP_BAD_COMMAND:
+
+			printf("Arduino received bad command\n");
+
+		break;
+
+
+
+		case RESP_BAD_RESPONSE:
+
+			printf("Arduino received unexpected response\n");
+
+		break;
+
+
+
+		default:
+
+			printf("Arduino reports a weird error\n");
+
+	}
+
+}
+
+
+
+void handleMessage(TPacket *packet)
+
+{
+
+	printf("Message from Alex: %s\n", packet->data);
+
+}
+
+
+
+void handlePacket(TPacket *packet)
+
+{
+
+	switch(packet->packetType)
+
+	{
+
+		case PACKET_TYPE_COMMAND:
+
+				// Only we send command packets, so ignore
+
+			break;
+
+
+
+		case PACKET_TYPE_RESPONSE:
+
+				handleResponse(packet);
+
+			break;
+
+
+
+		case PACKET_TYPE_ERROR:
+
+				handleErrorResponse(packet);
+
+			break;
+
+
+
+		case PACKET_TYPE_MESSAGE:
+
+				handleMessage(packet);
+
+			break;
+
+	}
+
+}
+
+
+
+void sendPacket(TPacket *packet)
+
+{
+
+	char buffer[PACKET_SIZE];
+
+	int len = serialize(buffer, packet, sizeof(TPacket));
+
+
+
+	serialWrite(buffer, len);
+
+}
+
+
+
+void *receiveThread(void *p)
+
+{
+
+	char buffer[PACKET_SIZE];
+
+	int len;
+
+	TPacket packet;
+
+	TResult result;
+
+	int counter=0;
+
+
+
+	while(1)
+
+	{
+
+		len = serialRead(buffer);
+
+		counter+=len;
+
+		if(len > 0)
+
+		{
+
+			result = deserialize(buffer, len, &packet);
+
+
+
+			if(result == PACKET_OK)
+
+			{
+
+				counter=0;
+
+				handlePacket(&packet);
+
+			}
+
+			else 
+
+				if(result != PACKET_INCOMPLETE)
+
+				{
+
+					printf("PACKET ERROR\n");
+
+					handleError(result);
+
+				}
+
+		}
+
+	}
+
+}
+
+
+
+void flushInput()
+
+{
+
+	char c;
+
+
+
+	while((c = getchar()) != '\n' && c != EOF);
+
+}
+
+
+
+void getParams(TPacket *commandPacket)
+
+{
+
+	printf("Enter distance/angle in cm/degrees (e.g. 50) and power in %% (e.g. 75) separated by space.\n");
+
+	printf("E.g. 50 75 means go at 50 cm at 75%% power for forward/backward, or 50 degrees left or right turn at 75%%  power\n");
+
+	scanf("%d %d", &commandPacket->params[0], &commandPacket->params[1]);
+
+	flushInput();
+
+}
+
+
+
+void sendCommand(char command)
+
+{
+
+	TPacket commandPacket;
+
+
+
+	commandPacket.packetType = PACKET_TYPE_COMMAND;
+
+
+
+	switch(command)
+
+	{
+
+		case 'f':
+
+		case 'F':
+
+			//getParams(&commandPacket);
+
+			commandPacket.command = COMMAND_FORWARD;
+
+			commandPacket.params[0] = 50;
+
+			commandPacket.params[1] = 100;
+
+			sendPacket(&commandPacket);
+
+			break;
+
+
+
+		case 'b':
+
+		case 'B':
+
+			//getParams(&commandPacket);
+
+			commandPacket.command = COMMAND_REVERSE;
+
+			commandPacket.params[0] = 50;
+
+			commandPacket.params[1] = 100;
+
+			sendPacket(&commandPacket);
+
+			break;
+
+
+
+		case 'l':
+
+		case 'L':
+
+			//getParams(&commandPacket);
+
+			commandPacket.command = COMMAND_TURN_LEFT;
+
+			commandPacket.params[0] = 50;
+
+			commandPacket.params[1] = 100;
+
+			sendPacket(&commandPacket);
+
+			break;
+
+
+
+		case 'r':
+
+		case 'R':
+
+			//getParams(&commandPacket);
+
+			commandPacket.command = COMMAND_TURN_RIGHT;
+
+			commandPacket.params[0] = 50;
+
+			commandPacket.params[1] = 100;
+
+			sendPacket(&commandPacket);
+
+			break;
+
+
+
+		case 's':
+
+		case 'S':
+
+			commandPacket.command = COMMAND_STOP;
+
+			sendPacket(&commandPacket);
+
+			break;
+
+
+
+		case 'c':
+
+		case 'C':
+
+			commandPacket.command = COMMAND_CLEAR_STATS;
+
+			commandPacket.params[0] = 0;
+
+			sendPacket(&commandPacket);
+
+			break;
+
+
+
+		case 'g':
+
+		case 'G':
+
+			commandPacket.command = COMMAND_GET_STATS;
+
+			sendPacket(&commandPacket);
+
+			break;
+
+
+
+		case 'q':
+
+		case 'Q':
+
+			exitFlag=1;
+
+			break;
+
+			
+
+		default:
+
+			printf("Bad command\n");
+
+
+
+	}
+
+}
+
+
+
+// Signal handler for graceful termination
+
+void signalHandler( int signum ) {
+
+    std::cout << "Interrupt signal (" << signum << ") received.\n";
+
+    // Cleanup and close up stuff here
+
+    
+
+    // Terminate program
+
+    exit(signum);  
+
+}
+
+
+
+int main()
+
+{
+
+	
+
+	// Connect to the Arduino
+
+	startSerial(PORT_NAME, BAUD_RATE, 8, 'N', 1, 5);
+
+
+
+	// Sleep for two seconds
+
+	printf("WAITING TWO SECONDS FOR ARDUINO TO REBOOT\n");
+
+	sleep(2);
+
+	printf("DONE\n");
+
+
+
+	// Spawn receiver thread
+
+	pthread_t recv;
+
+
+
+	pthread_create(&recv, NULL, receiveThread, NULL);
+
+
+
+	// Send a hello packet
+
+	TPacket helloPacket;
+
+
+
+	helloPacket.packetType = PACKET_TYPE_HELLO;
+
+	sendPacket(&helloPacket);
+
+	
+
+	//------------------------------------------------------------------
+
+	//setup the TCP server
+
+	signal(SIGINT, signalHandler);
+
+
+
+    int server_fd, new_socket;
+
+    struct sockaddr_in address;
+
+    int opt = 1;
+
+    int addrlen = sizeof(address);
+
+    const int PORT = 12345; // Port number where the server will listen
+
+
+
+    // Creating socket file descriptor
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+
+        perror("socket failed");
+
+        exit(EXIT_FAILURE);
+
+    }
+
+    
+
+    // Forcefully attaching socket to the port 
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+
+        perror("setsockopt");
+
+        exit(EXIT_FAILURE);
+
+    }
+
+    address.sin_family = AF_INET;
+
+    address.sin_addr.s_addr = INADDR_ANY; //inet_addr(PI_IP);
+
+    address.sin_port = htons(PORT);
+
+    
+
+    // Forcefully attaching socket to the port 
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+
+        perror("bind failed");
+
+        exit(EXIT_FAILURE);
+
+    }
+
+    if (listen(server_fd, 3) < 0) {
+
+        perror("listen");
+
+        exit(EXIT_FAILURE);
+
+    }
+
+
+
+    std::cout << "Server listening on " << PI_IP << ":" << PORT << std::endl;
+
+    
+
+     while(true) {
+
+        //char buffer[2] = {0};
+
+        
+
+        std::cout << "Waiting for a connection..." << std::endl;
+
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+
+            perror("accept");
+
+            continue; // Continue to the next iteration of the loop
+
+        }
+
+        
+
+        std::cout << "Connection accepted" << std::endl;
+
+        
+
+        // This inner loop reads data sent by a connected client
+
+        while(true) {
+
+	    const char* response = "c";
+
+            send(new_socket, response, strlen(response), 0);
+
+	    char buffer[2] = {0}; 
+
+            ssize_t bytes_read = read(new_socket, buffer, 1);
+
+            if (bytes_read <= 0) {
+
+                // If read returns 0, the client has closed the connection
+
+                // If read returns -1, an error occurred
+
+		std::cout << "Read error or connection closed by client" << std::endl;
+
+                break;
+
+            }
+
+	    //add something to check if there is data in buffer[0]
+
+            std::cout << "Sending command: " << buffer[0] << std::endl;
+
+            sendCommand(buffer[0]);
+
+            //memset(buffer, 0, sizeof(buffer)); // Clear the buffer
+
+        }
+
+        
+
+        close(new_socket);
+
+        std::cout << "Connection closed" << std::endl;
+
+    }
+
+    
+
+    // The program should never reach here in normal operation
+
+    close(server_fd);
+
+    
+
+
+
+	printf("Closing connection to Arduino.\n");
+
+	endSerial();
+
+}
+
