0192d0cabf2541940c74a7ebec5c6776d1df3711
Isaac Saw <141603285+isaacsaw25@users.noreply.github.com>
2024-04-10 10:02:11 +0800

    Update ArduinoUNO.ino for serialComms

--- ArduinoUNO/ArduinoUNO.ino
+++ ArduinoUNO/ArduinoUNO.ino
@@ -1,4 +1,14 @@
+#include <serialize.h>
+#include <stdarg.h>
+#include <math.h>
+#include "packet.h"
+#include "constants.h"
+
 bool serialOn = true;
+int redFrequency = 0;
+int greenFrequency = 0;
+int blueFrequency = 0;
+volatile unsigned long ultrasonicDistances[4] = { 0 }; // Array to store distance readings
 
 void setup() {
   setupTimer();
@@ -9,4 +19,138 @@ void setup() {
 
 void loop() {
   // No loop as the interval is triggered by ISR
+}
+
+TResult readPacket(TPacket *packet) {
+  // Reads in data from the serial port and
+  // deserializes it.Returns deserialized
+  // data in "packet".
+
+  char buffer[PACKET_SIZE];
+  int len;
+
+  len = readSerial(buffer);
+
+  if (len == 0)
+    return PACKET_INCOMPLETE;
+  else
+    return deserialize(buffer, len, packet);
+}
+
+void sendMessage(const char *message) {
+  // Sends text messages back to the Pi. Useful
+  // for debugging.
+
+  TPacket messagePacket;
+  messagePacket.packetType = PACKET_TYPE_MESSAGE;
+  strncpy(messagePacket.data, message, MAX_STR_LEN);
+  sendResponse(&messagePacket);
+}
+
+void dbprintf(char *format, ...) {
+  va_list args;
+  char buffer[128];
+  va_start(args, format);
+  vsprintf(buffer, format, args);
+  sendMessage(buffer);
+}
+
+void sendBadPacket() {
+  // Tell the Pi that it sent us a packet with a bad
+  // magic number.
+
+  TPacket badPacket;
+  badPacket.packetType = PACKET_TYPE_ERROR;
+  badPacket.command = RESP_BAD_PACKET;
+  sendResponse(&badPacket);
+}
+
+void sendBadChecksum() {
+  // Tell the Pi that it sent us a packet with a bad
+  // checksum.
+
+  TPacket badChecksum;
+  badChecksum.packetType = PACKET_TYPE_ERROR;
+  badChecksum.command = RESP_BAD_CHECKSUM;
+  sendResponse(&badChecksum);
+}
+
+void sendBadCommand() {
+  // Tell the Pi that we don't understand its
+  // command sent to us.
+
+  TPacket badCommand;
+  badCommand.packetType = PACKET_TYPE_ERROR;
+  badCommand.command = RESP_BAD_COMMAND;
+  sendResponse(&badCommand);
+}
+
+void sendBadResponse() {
+  TPacket badResponse;
+  badResponse.packetType = PACKET_TYPE_ERROR;
+  badResponse.command = RESP_BAD_RESPONSE;
+  sendResponse(&badResponse);
+}
+
+void sendOK() {
+  TPacket okPacket;
+  okPacket.packetType = PACKET_TYPE_RESPONSE;
+  okPacket.command = RESP_OK;
+  sendResponse(&okPacket);
+}
+
+void sendResponse(TPacket *packet) {
+  // Takes a packet, serializes it then sends it out
+  // over the serial port.
+  char buffer[PACKET_SIZE];
+  int len;
+
+  len = serialize(buffer, packet, sizeof(TPacket));
+  writeSerial(buffer, len);
+}
+
+int readSerial(char *buffer)
+{
+
+  int count = 0;
+
+  // Change Serial to Serial2/Serial3/Serial4 in later labs when using other UARTs
+
+  while (Serial.available())
+    buffer[count++] = Serial.read();
+
+  return count;
+}
+
+// Write to the serial port. Replaced later with
+// bare-metal code
+
+void writeSerial(const char *buffer, int len)
+{
+  Serial.write(buffer, len);
+  // Change Serial to Serial2/Serial3/Serial4 in later labs when using other UARTs
+}
+
+void waitForHello() {
+  int exit = 0;
+
+  while (!exit) {
+    TPacket hello;
+    TResult result;
+
+    do {
+      result = readPacket(&hello);
+    } while (result == PACKET_INCOMPLETE);
+
+    if (result == PACKET_OK) {
+      if (hello.packetType == PACKET_TYPE_HELLO) {
+        sendOK();
+        exit = 1;
+      } else
+        sendBadResponse();
+    } else if (result == PACKET_BAD) {
+      sendBadPacket();
+    } else if (result == PACKET_CHECKSUM_BAD)
+      sendBadChecksum();
+  }  // !exit
 }
